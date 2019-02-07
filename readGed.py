649f885677cb94031655a31c7adb5a91a22abad4
chloequinto <cquinto@stevens.edu>
2019-02-07 16:28:20 -0500

    cleaned it up

--- readGed.py
+++ readGed.py
@@ -1,7 +1,10 @@
 '''To Do 
 1. Check that it works 
-2. Try and Exception for inputs 
-3. Unit Tests 
+2. Try and Exception for inputs [x] 
+3. 
+4. 
+5. 
+6. Unit Tests 
 '''
 
 valid = {
@@ -9,15 +12,8 @@ valid = {
     "1": ("NAME", "SEX", "BIRT", "DEAT", "DIV", "FAMC", "FAMS", "MARR", "HUSB", "WIFE", "CHIL"),
     "2": ("DATE")
 }
-try:
-    inputGed = open("chloequinto.ged", "r")
-
-    writeOutput = open("output.txt", "w")
-
-    print("Opening files")
-except FileNotFoundError:
-    print("Cannot open file")
-else:
+def parser(inputGed): 
+    global writeOutput
     for i in inputGed:
         writeOutput.write("--> " + i)
         inputSplit = i.strip().split(maxsplit=2)
@@ -32,8 +28,21 @@ else:
         elif inputSplit[0] in valid:
             if inputSplit[1] in valid[inputSplit[0]]:
                 validLine = "Y"
-
         writeOutput.write("<-- " + inputSplit[0] + "|" + inputSplit[1] + "|" + validLine + "|" + " ".join(inputSplit[2:]) + "\n")
-
-
     print("All done!")
+
+def main(): 
+    global inputGed, writeOutput
+    try:
+        inputGed = open("input.ged", "r")
+        writeOutput = open("output.txt", "w")
+
+        print("Opening files")
+    except FileNotFoundError:
+        print("Cannot open file")
+    else:
+        parser(inputGed)
+
+
+if __name__ == "__main__": 
+    main()
