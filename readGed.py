f4b658e26550b24de167b43d97728eca50140a13
rzhou10 <rzhou10@gmail.com>
2019-02-08 11:37:29 -0500

    able to add ids to individual_table

--- readGed.py
+++ readGed.py
@@ -6,14 +6,26 @@
 6. Unit Tests 
 '''
 from prettytable import PrettyTable 
+
 valid = {
     "0": ("HEAD", "TRLR", "NOTE"),
     "1": ("NAME", "SEX", "BIRT", "DEAT", "DIV", "FAMC", "FAMS", "MARR", "HUSB", "WIFE", "CHIL"),
     "2": ("DATE")
 }
 
-# Individual_Table = {}
-# Family_Table = {}
+Individual_Table = {
+    'ID': [],
+    'Name': [],
+    'Gender': [],
+    'Birthday': [],
+    'Age': [], 
+    'Alive': [],
+    'Death': [],
+    'Child': [],
+    'Spouse': []
+}
+
+Family_Table = {}
 
 def parser(inputGed): 
     global writeOutput
@@ -33,22 +45,34 @@ def parser(inputGed):
                 validLine = "Y"
         writeOutput.write("<-- " + inputSplit[0] + "|" + inputSplit[1] + "|" + validLine + "|" + " ".join(inputSplit[2:]) + "\n")
     print("All done!")
-#Function to parse through and store families and individuals 
-# def fam(): 
-    #TO DO 
-    #Parse through the ged file for IDs
-    # append to Individual_Table or Family_Table
 
+#Function to parse through and store families and individuals
+def fam(inputGed):
+    print("hello")
+    for k, i in enumerate(inputGed):
+        line = i.strip().split(maxsplit=2)
+        if len(line) > 2 and line[2] == "INDI":
+            print(line)
+            if "ID" in Individual_Table:
+                Individual_Table["ID"].append(line[1])
+
+            while line[2] != "INDI":
+                if line[1] in Individual_Table:
+                    Individual_Table[line[1]].append(line[2])
+                k += 1
+
+
+    print(Individual_Table)
 
 #Function to write the table 
 def table(): 
     x = PrettyTable()
     x.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 
                     'Alive', 'Death', 'Child', 'Spouse']
-    #UNCOMMENT 
-    # for i in Individual_Table.items(): 
-    #     x.add_row([i.ID, i.NAME, i.GENDER, i.BIRTHDAY, i.AGE, 
-    #     i.ALIVE, i.DEATH, i.CHILD, i.SPOUSE ])
+
+    for i in Individual_Table.items(): 
+        x.add_row([i.ID, i.NAME, i.GENDER, i.BIRTHDAY, i.AGE, 
+        i.ALIVE, i.DEATH, i.CHILD, i.SPOUSE ])
     print("\nIndividuals")
     print(x)
     
@@ -56,10 +80,10 @@ def table():
     y.field_names = ['ID', 'Married', 'Divorced', 'Husband ID',
                     'Husband Name', 'Wife ID', 'Wife Name', 'Children']
     print("Families")  
-    #UNCOMMENT 
-    # for j in Family_Table.items():
-    #     y.add_row([j.ID, j.MARRIED, i.DIVOCED, i.HUSBANDid, 
-    #     i.HUSBANDname, i.WIFEid, i.WIFEname, i.CHILDREN])
+
+    for j in Family_Table.items():
+        y.add_row([j.ID, j.MARRIED, i.DIVORCED, i.HUSBANDid, 
+        i.HUSBANDname, i.WIFEid, i.WIFEname, i.CHILDREN])
     print(y) 
 
 def main(): 
@@ -68,15 +92,13 @@ def main():
         inputGed = open("input.ged", "r")
         print("Opening files")
         writeOutput = open("output.txt", "w")
-    
         
     except FileNotFoundError:
         print("Cannot open file")
     else:
-        parser(inputGed)
-        table()
+        fam(inputGed)
+        # table()
     
-
 
 if __name__ == "__main__": 
     main()
