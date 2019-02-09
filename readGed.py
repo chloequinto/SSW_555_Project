a83728a3085945d5e6db7cb5767da8dbf24df586
chloequinto <cquinto@stevens.edu>
2019-02-09 17:53:08 -0500

    tables are up

--- readGed.py
+++ readGed.py
@@ -37,13 +37,13 @@ Family_Table = {
     'CHIL': []
 }
 
+
 def parser(inputGed): 
     global writeOutput
     for i in inputGed:
         writeOutput.write("--> " + i)
         inputSplit = i.strip().split(maxsplit=2)
         validLine = "N"
-
         if len(inputSplit) > 2 and inputSplit[2] in ["INDI", "FAM"]:
             if inputSplit[0] == "0":
                 temp = inputSplit[2]
@@ -56,60 +56,67 @@ def parser(inputGed):
         writeOutput.write("<-- " + inputSplit[0] + "|" + inputSplit[1] + "|" + validLine + "|" + " ".join(inputSplit[2:]) + "\n")
     print("All done!")
 
+
 #Function to parse through and store families and individuals
 def fam(inputGed):
-    for i in inputGed: 
+    indi_list = []
+    fam_list = []
+    indi_data = []
+    fam_data = []
+    new_individual = 0 #used to check if new individual 
+    # if new_individual = 0 -> true 
+    # if new_individual = 1 -> false -> append old values 
+    new_family = 0 #used to check if new family 
+    for i in inputGed:
         line = i.strip().split(maxsplit=2)
-        if len(line) > 2 and line[2] == "INDI":
-            Individual_Table["ID"].append(line[1])
-        elif line[0] == "1" and line[1] == "NAME": 
-            Individual_Table["NAME"].append(line[2])
-        elif line[0] == "1" and line[1] == "SEX":
-            Individual_Table["SEX"].append(line[2])
-        # elif line[0] == "1" and line[1] == "BIRT" :
-        #     # add date somehow 
-        #     if line[1] == "DEAT":
-        #         #read death day to 
-        #         Individual_Table["ALIVE"].append("") #date added
-        #     else:
-        #         Individual_Table["ALIVE"].append("NA")
-        elif line[0] == "1" and line[1] == "DEAT": # you reach a problem here
-            if line[2] == "Y": 
-                Individual_Table["ALIVE"].append("False")
-            else:
-                Individual_Table["ALIVE"].append("TRUE")
-        elif line[0] == "1" and (line[1] == "HUSB" or line[1] == "WIFE"):
-            Individual_Table["SPOUSE"].append(line[2])
-        elif line[0] == "1" and (line[1] == "CHIL"): #you reach a problem here
-            #not every individual has a child
-            #need another parameter to check for individuals
-            #propose a different data structure and possibly have pointers 
-            Individual_Table["CHIL"].append(line[2])
+        if len(line) > 2 and line[0] == str(0):
+            if new_individual == 1: #if this is an instance that we see a new person
+                indi_list.append(indi_data) 
+                indi_data = []
+                new_individual = 0
+            elif new_family == 1: #if this is an instance that we see a new family 
+                fam_list.append(fam_data)
+                fam_data = []
+                new_family = 0 
+            elif line[2] == "INDI" and new_individual == 0: 
+                indi_data.append(line[1])
+                new_individual = 1 
+            elif line[2] == "FAM" and new_individual == 0: 
+                new_family = 1
+                fam_data.append(line[1])
+            else: 
+                pass
+           
+        elif line[0] == str(1): 
+            if line[1] == "NAME": 
+                indi_data.append(line[2])
+            elif line[1] == "SEX": 
+                indi_data.append(line[2])
+            elif line[1] == "BIRT":
+                indi_data.append("12 27 1997")
+                indi_data.append("21")
+                indi_data.append("Y")
+            elif line[1] == "FAMS": 
+                indi_data.append(line[2])
+            elif line[1] == "FAMC": 
+                indi_data.append(line[2])
+            
+    # print(indi_list)
+    return indi_list
 
-    print(Individual_Table)
-
-def split_them_lines(lines): 
-    '''Function to split the given line into the corresponding elements ''' 
-    #necessary for getting that death year compare to datetime
-    b = line.split(' ', 2)
-    level = b[0]
-    tag = b[1].strip()
-    if (b[2] != ''): 
-        arg = b[2].strip()
-    else: 
-        arg = ''
-    return [level, tag, arg]
-    
-
+           
+                    
 #Function to write the table 
-def table(): 
+def table(indi_list): 
     x = PrettyTable()
     x.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 
                     'Alive', 'Death', 'Child', 'Spouse']
 
-    for i in Individual_Table.items(): 
-        x.add_row([i.ID, i.NAME, i.GENDER, i.BIRTHDAY, i.AGE, 
-        i.ALIVE, i.DEATH, i.CHILD, i.SPOUSE ])
+    for i in indi_list: 
+
+        x.add_row([i[0], i[1], i[2], i[3], i[4], 
+        i[0], i[0], i[0], i[0] ])
+        # print(i[0])
     print("\nIndividuals")
     print(x)
     
@@ -118,23 +125,23 @@ def table():
                     'Husband Name', 'Wife ID', 'Wife Name', 'Children']
     print("Families")  
 
-    for j in Family_Table.items():
-        y.add_row([j.ID, j.MARRIED, i.DIVORCED, i.HUSBANDid, 
-        i.HUSBANDname, i.WIFEid, i.WIFEname, i.CHILDREN])
+    # for j in Family_Table.items():
+    #     y.add_row([j.ID, j.MARRIED, i.DIVORCED, i.HUSBANDid, 
+    #     i.HUSBANDname, i.WIFEid, i.WIFEname, i.CHILDREN])
     print(y) 
 
 def main(): 
     global inputGed, writeOutput
     try:
         inputGed = open("input.ged", "r")
-        print("Opening files")
+        # print("Opening files")
         writeOutput = open("output.txt", "w")
         
     except FileNotFoundError:
         print("Cannot open file")
     else:
-        fam(inputGed)
-        # table()
+        # fam(inputGed)
+        table(fam(inputGed))
     
 
 if __name__ == "__main__": 
