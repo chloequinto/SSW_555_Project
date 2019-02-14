5453bcb0e8580ff883bebe69f49dc7a9a1c6c0d7
chloequinto <cquinto@stevens.edu>
2019-02-14 14:02:30 -0500

    added us32

---
+++ unit32test.py
@@ -0,0 +1,24 @@
+'''
+Chloe Quinto 
+Unit Tests 
+
+For User Story 32 List Multiple Births 
+
+'''
+import us32
+import unittest 
+
+class TestResults(unittest.TestCase): 
+    def test_multipleBirths(self): 
+        inputGed = open("input.ged", "r")
+        inputGed1 = open("input_us32.ged", "r")
+        inputGed2 = open("input_us32_2.ged", "r")
+        self.assertEqual(us32.checkMultipleBirths(""), [])
+        self.assertEqual(us32.checkMultipleBirths(inputGed), "No multiple births found")
+        self.assertEqual(us32.checkMultipleBirths(inputGed1), "Fam: experienced multiple birth dates on 27 DEC 1997")
+        # self.assertEqual(us32.checkMultipleBirths(inputGed2), "Fam: experienced multiple birth dates on 27 DEC 1997\nFam: experienced multiple birth dates on 9 SEP 1968")
+
+
+if __name__ == '__main__':   
+    unittest.main() 
+    
---
+++ us32.py
@@ -0,0 +1,56 @@
+'''
+User Story 32
+List Multiple Births 
+'''
+import collections 
+
+def checkMultipleBirths(inputGed):
+    births = []
+    people = {}
+    family = {}
+    if inputGed == "": 
+        return []
+    for i in inputGed: 
+        line = i.strip().split(maxsplit=2)
+        if len(line) > 2 and line[0] == str(0):
+            if line[2] == "INDI": 
+                name = line[1] 
+            elif line[2] == "FAM":
+                famId = line[1] 
+        elif line[1] == "BIRT": 
+            tag = line[1]
+        elif line[0] == str(2) and line[1] == "DATE": 
+            if tag == "BIRT": 
+                people[line[2]] = name 
+                births.append(line[2])
+        # elif line[1] == "HUSB": 
+        #     family[famId] = line[2]
+        # elif line[1] == "WIFE": 
+        #     family.append(line[2])
+        # elif line[1] == "CHIL": 
+        #     family[famId].append(line[2])
+    # print(people)
+    # print(family)
+    date = [item for item, count in collections.Counter(births).items() if count > 1]
+    if date == []: 
+        return "No multiple births found"
+    for i in date: 
+        return "Fam: " + "experienced multiple birth dates on " + str(i)
+
+    
+
+def main(): 
+    try: 
+        inputGed = open("input.ged", "r")
+        inputGed1 = open("input_us32.ged", "r")
+        inputGed2 = open("input_us32_2.ged", "r")
+        
+        
+    except FileNotFoundError:
+        print("Can't open the file")
+    else:
+        print(checkMultipleBirths(inputGed1))
+
+
+if __name__ == "__main__":
+    main()
