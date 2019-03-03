0e3a73cb78411f50736e2c7f6a1a9f84b2f9874d
rzhou10 <rzhou10@gmail.com>
2019-03-03 18:26:49 -0500

    us29 outputs on master

--- us29.py
+++ us29.py
@@ -10,20 +10,7 @@ def deaths(input):
     deaths = []
     for i in input:
         if i[6] != "NA":
+            print("Individual " + i[0] + " is dead.")
             deaths.append(i)
     
     return deaths
-
-#Function to write the table 
-def table(lists):
-    x = PrettyTable()
-    x.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age',
-                     'Alive', 'Death', 'Child', 'Spouse']
-
-    for i in lists:
-        x.add_row([i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8]])
-    # print("\nDeaths")
-    # print(x)
-
-def main(lists):
-    table(deaths(lists))
