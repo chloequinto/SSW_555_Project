00432e137ae7542863ccf14b381f3a38731c1d98
stdlibrainbow <1013222570@qq.com>
2019-03-04 18:46:32 -0500

    update as master

--- us02.py
+++ us02.py
@@ -7,7 +7,7 @@ Birth before marriage
 from datetime import date
 from datetime import datetime
 from collections import OrderedDict
-import unittest
+
 
 individual = OrderedDict()
 family = OrderedDict()
@@ -146,25 +146,7 @@ def BirthBeforeMarriage(individual):
         return True
 
 
-class TestResults(unittest.TestCase):
-    def test_birthBeforeMarriage(self):
-        inputGed = open("inputGed5.ged", "r")
-        for indi in individual:
-            self.assertTrue(BirthBeforeMarriage(individual[indi]),msg="ERROR: INDIVIDUAL: US02: "+ individual[indi].ID + ": Birthday " + individual[indi].birthDate + " occurs before marriage " + individual[indi].marriageDate)
-
-
-def main():
-    try:
-        inputGed = open("inputGed5.ged", "r")
-
-    except FileNotFoundError:
-        print("Can't open the file")
-    else:
-        individual = parseGed(inputGed)
-        return individual
-        
-
-
-if __name__ == '__main__':
-    main()
-    unittest.main()
+def main(): 
+    inputGed = open("Sprint1.ged", "r")
+    individual = parseGed(inputGed)
+    return individual
