45937755c9e002524e87c6cdea765e9f79cf5202
rzhou10 <rzhou10@gmail.com>
2019-03-06 14:33:46 -0500

    gitignore for sprint2

--- us14.py
+++
@@ -1,9 +0,0 @@
-'''
-Unit Test for US14
-No more than 5 siblings should be born at the same time 
-'''
-def checkForMoreThan5Sibs(input): 
-
-
-def main(lists): 
-    checkForMoreThan5Sibs(lists[0], lists[1])
