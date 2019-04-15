fb3858ecdfe2c11a42efe0400ac2ce45f0fe2d11
chloequinto <cquinto@stevens.edu>
2019-04-15 14:31:24 -0400

    us17

--- unit_test.py
+++ unit_test.py
@@ -2,7 +2,7 @@
 All Unit Tests 
 '''
 
-from package.userStories import us01, us02, us03, us04, us05, us06, us07, us08, us09, us10, us12, us13, us14, us15, us16, us18, us21, us22, us23, us24, us25, us26, us27, us28, us29, us30, us31, us32, us33, us34, us35, us36, us38, us40, us41, us42, us20, us39
+from package.userStories import us01, us02, us03, us04, us05, us06, us07, us08, us09, us10, us12, us13, us14, us15, us16, us17, us18, us21, us22, us23, us24, us25, us26, us27, us28, us29, us30, us31, us32, us33, us34, us35, us36, us38, us40, us41, us42, us20, us39
 import unittest
 import readGed
 
@@ -344,6 +344,11 @@ class TestResults(unittest.TestCase):
         for i in family:
             temp = us20.NotMarryNiecesAndNephews(family[i], family, individual)
             self.assertTrue(temp[0])
+    
+    def testUS17(self): 
+        inputGed = open("data/Sprint4.ged", "r")
+        allLists = readGed.fam(inputGed)
+        self.assertEqual(us17.main(allLists[1], allLists[3]), None)
 
 
 if __name__ == '__main__':
